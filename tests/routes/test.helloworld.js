a6a5b8d88188eab0878fea6495d014a9bb7af9b4
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2016-12-09 21:50:55 -0600

    add tests for helloworld

---
+++ tests/routes/test.helloworld.js
@@ -0,0 +1,29 @@
+var request = require('supertest');
+var should = require("should");
+
+var app = require("./app");
+
+describe('Test helloworld routes', function() {
+  before(function(done) {
+    server = app.listen(done);
+	});
+
+  after(function() {
+		server.close();
+	});
+
+  describe('Hello World API', function () {
+    it('should call to GET /api/v1/helloworld', function(done) {
+			function check(err, data) {
+				var results = data.body;
+        results.should.be.type('object');
+				done(err);
+			};
+
+			request(server)
+				.get('/api/v1/helloworld')
+        .set('Accept', 'application/json')
+				.expect(200, check);
+		});
+  }
+}
